9c05913fe94ba05acc1b34f3ac34d4cf57fc2d45
carollaginestra <carolina.laginestra@driven.cx>
2020-10-26 12:06:39 -0300

    organization possibilities

---
+++ resposta_questao_5.js
@@ -0,0 +1,12 @@
+//qty
+const friends = 5;
+
+const combinations = (qty) => {
+  //check for options
+  if(qty > 1) {
+    if(qty > 2) qty*= qty--
+  
+    return qty
+  }
+}
+console.log(combinations(friends))
